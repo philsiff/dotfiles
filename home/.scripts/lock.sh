08841390f7e81b56b988315a704a07bcac2aa8bd
philsiff <filman74@verizon.net>
2017-02-16 12:16:44 -0500

    Added gaps and terminal transparency

--- home/.scripts/lock.sh
+++ home/.scripts/lock.sh
@@ -6,6 +6,6 @@ tmpbg='/tmp/screen.png'
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" -scale 20% -scale 2000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock -u -i "$tmpbg"
